4da30e62aa2a67f79d2a6819730585aa7c9bd410
marcelobonfim <marceloras@gmail.com>
2023-08-17 22:42:11 -0300

    Iniciando com RabbitMQ e Spring AMQP

--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -6,7 +6,6 @@ import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.services.UserCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -24,16 +23,5 @@ public class UserCourseServiceImpl implements UserCourseService {
     @Override
     public UserCourseModel save(UserCourseModel userCourseModel) {
         return userCourseRepository.save(userCourseModel);
-    }
-
-    @Override
-    public boolean existsByCourseId(UUID courseId) {
-        return userCourseRepository.existsByCourseId(courseId);
-    }
-
-    @Transactional
-    @Override
-    public void deleteUserCourseByCourse(UUID courseId) {
-        userCourseRepository.deleteAllByCourseId(courseId);
     }
 }
